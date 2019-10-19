87490a8b2943b775be14f2e1996e2683cca892db
DRC-DEV <dracarys.pro@gmail.com>
2019-10-19 05:54:15 +0200

    Remove debug messages

--- src/main/java/pro/dracarys/LocketteX/api/LocketteXAPI.java
+++ src/main/java/pro/dracarys/LocketteX/api/LocketteXAPI.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class LocketteXAPI {
 
 
-    public static boolean hasChestAccess(Player p, InventoryHolder holder){
+    public static boolean hasChestAccess(Player p, InventoryHolder holder) {
         String owner = getChestOwner(holder);
         if (owner != null && owner.equalsIgnoreCase(p.getName()))
             return true;
@@ -25,14 +25,14 @@ public class LocketteXAPI {
     public static String getChestOwner(InventoryHolder holder) {
         List<Block> chestBlocks = new ArrayList<>();
         if (holder instanceof DoubleChest) {
-            Util.sendConsole("DoubleChest -> >");
+            //Util.sendConsole("&a<DEBUG> &fDetected: DoubleChest");
             DoubleChest dchest = (DoubleChest) holder;
             Chest chest1 = (Chest) dchest.getRightSide();
             chestBlocks.add(chest1.getBlock());
             Chest chest2 = (Chest) dchest.getLeftSide();
             chestBlocks.add(chest2.getBlock());
         } else if (holder instanceof Chest) {
-            Util.sendConsole("Chest -> >");
+            //Util.sendConsole("&a<DEBUG> &fDetected: Chest");
             Chest chest = (Chest) holder;
             chestBlocks.add(chest.getBlock());
         } else {
@@ -42,14 +42,13 @@ public class LocketteXAPI {
         for (Block chestBlock : chestBlocks) {
             for (Block block : Util.getBlocks(chestBlock, 1)) {
                 if (block.getType().equals(Material.WALL_SIGN)) {
-                    Util.sendConsole("detected -> > SIGN");
+                    //Util.sendConsole("&a<DEBUG> &fDetected: SIGN");
                     Sign s = (Sign) block.getState();
                     org.bukkit.material.Sign sd = (org.bukkit.material.Sign) s.getData();
                     if (chestBlock.getLocation().equals(block.getRelative(sd.getAttachedFace()).getLocation())) {
                         if (s.getLine(0).contains(Util.color("&1[Protect]"))) {
-                            Util.sendConsole("protect-sign detected =="+s.getLine(1));
-                            if (s.getLine(1).length() <3) { //Playernames can't be less than 3 digits
-                                Util.sendConsole("line length <3 ="+s.getLine(1).length());
+                            //Util.sendConsole("&a<DEBUG> &fDetected: ProtectedSign"+s.getLine(1));
+                            if (s.getLine(1).length() < 3) { //Playernames can't be less than 3 digits
                                 return null;
                             }
                             return (s.getLine(1));
